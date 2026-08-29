351471c836e6001526b85fddda02038cb7cde948
Aleexender <alexkjh2011@gmail.com>
2026-08-29 19:18:35 +0900

    refactor: rename ploutos_be to ploutos

--- src/main/java/org/example/ploutos/PloutosApplication.java
+++ src/main/java/org/example/ploutos/PloutosApplication.java
@@ -1,13 +1,13 @@
-package org.example.ploutos_be;
+package org.example.ploutos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PloutosBeApplication {
+public class PloutosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PloutosBeApplication.class, args);
+		SpringApplication.run(PloutosApplication.class, args);
 	}
 
 }
--- src/test/java/org/example/ploutos/PloutosApplicationTests.java
+++ src/test/java/org/example/ploutos/PloutosApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.ploutos_be;
+package org.example.ploutos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PloutosBeApplicationTests {
+class PloutosApplicationTests {
 
 	@Test
 	void contextLoads() {
