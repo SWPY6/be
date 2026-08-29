6b7c1ac4c4048874a16c056d507f36454e102511
Aleexender <alexkjh2011@gmail.com>
2026-08-29 19:11:44 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/org/example/ploutos_be/PloutosBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ploutos_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloutosBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloutosBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ploutos_be/PloutosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ploutos_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloutosBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
